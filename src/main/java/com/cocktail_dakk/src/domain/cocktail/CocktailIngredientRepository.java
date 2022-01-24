ef53e4ea2df2d59c6d5ef1fcd56d3410e51a6f9e
BeobSik <tjswl756@naver.com>
2022-01-24 11:48:43 +0900

    refactor : delete ingredient entity

--- src/main/java/com/cocktail_dakk/src/domain/cocktail/CocktailIngredientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cocktail_dakk.src.domain.cocktail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CocktailIngredientRepository extends JpaRepository<CocktailIngredient, Long> {
-}
