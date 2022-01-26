32914ece52444a59f23048b9ec00f271da553043
BeobSik <tjswl756@naver.com>
2022-01-26 15:20:51 +0900

    fix : modify typo

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/app/cocktaildakk
+REPOSITORY=/home/ubuntu/app/cocktaildakk/build/libs
 cd $REPOSITORY
 
 APP_NAME=cocktail_dakk
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/*.jar | tail -n 1)
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
@@ -20,5 +19,5 @@ fi
 
 
 
-echo "> $JAR_PATH 배포"
-nohup java -jar \ -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-real-db.properties \  $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_NAME 배포"
+nohup java -jar \ -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-real-db.properties \  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
