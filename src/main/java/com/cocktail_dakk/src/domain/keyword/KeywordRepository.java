3224dc9114a58749f37747585184b2f7e0ec43aa
BeobSik <tjswl756@naver.com>
2022-01-30 00:05:57 +0900

    feat : add search with keyword

--- src/main/java/com/cocktail_dakk/src/domain/keyword/KeywordRepository.java
+++ src/main/java/com/cocktail_dakk/src/domain/keyword/KeywordRepository.java
@@ -1,6 +1,9 @@
 package com.cocktail_dakk.src.domain.keyword;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    Page<Keyword> findAllByKeywordNameContaining(Pageable pageable, String keywordName);
 }
--- src/main/java/com/cocktail_dakk/src/service/SearchCocktailInfoService.java
+++ src/main/java/com/cocktail_dakk/src/service/SearchCocktailInfoService.java
@@ -4,6 +4,8 @@ import com.cocktail_dakk.config.BaseException;
 import com.cocktail_dakk.config.BaseResponseStatus;
 import com.cocktail_dakk.src.domain.cocktail.CocktailInfoRepository;
 import com.cocktail_dakk.src.domain.cocktail.dto.SearchCocktailInfoRes;
+import com.cocktail_dakk.src.domain.keyword.Keyword;
+import com.cocktail_dakk.src.domain.keyword.KeywordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -23,11 +25,22 @@ public class SearchCocktailInfoService{
     @Autowired
     CocktailInfoRepository cocktailInfoRepository;
 
+    @Autowired
+    KeywordRepository keywordRepository;
+
     public Page<SearchCocktailInfoRes> findByInputStrAll(Pageable pageable, String inputStr) throws BaseException{
         try {
             List<SearchCocktailInfoRes> cocktailInfoResList = cocktailInfoRepository.findAllByKoreanNameContainingOrEnglishNameContainingOrIngredientContaining(pageable, inputStr, inputStr, inputStr)
                     .stream().map(SearchCocktailInfoRes::new)
                     .collect(Collectors.toList());
+
+            Page<Keyword> allByKeywordNameContaining = keywordRepository.findAllByKeywordNameContaining(pageable, inputStr);
+
+            allByKeywordNameContaining.forEach(keyword -> {
+                keyword.getCocktailKeywords().forEach(cocktailKeyword -> {
+                    cocktailInfoResList.add(new SearchCocktailInfoRes(cocktailKeyword.getCocktailInfo()));
+                });
+            });
 
             return new PageImpl<>(cocktailInfoResList);
         }catch (Exception exception){
