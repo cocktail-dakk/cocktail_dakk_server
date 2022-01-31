a377234a71764a4247f6cc301ee4c6c23340c5b9
ahnjungeun <616a65@gmail.com>
2022-01-31 20:14:47 +0900

    fix : add http mapping param

--- src/main/java/com/cocktail_dakk/src/controller/CocktailDetailInfoController.java
+++ src/main/java/com/cocktail_dakk/src/controller/CocktailDetailInfoController.java
@@ -5,7 +5,9 @@ import com.cocktail_dakk.src.domain.cocktail.dto.CocktailDetailsInfoRes;
 import com.cocktail_dakk.src.service.CocktailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,7 +17,8 @@ public class CocktailDetailInfoController {
     @Autowired
     private final CocktailService cocktailService;
 
-    public CocktailDetailsInfoRes getCocktailInfo(Long id){
+    @GetMapping("/details")
+    public CocktailDetailsInfoRes getCocktailInfo(@RequestParam Long id){
         return cocktailService.getCocktailDetailsInfo(id);
     }
 
