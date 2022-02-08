07ae341cac99ca5f8a843779ea74a2f4661aa07c
BeobSik <tjswl756@naver.com>
2022-02-08 20:46:53 +0900

    feat: chagne alcoholLevel to minAlcoholLevel and maxAlcoholLevel at Filter

--- src/main/java/com/cocktail_dakk/src/domain/cocktail/CocktailFilterRepository.java
+++ src/main/java/com/cocktail_dakk/src/domain/cocktail/CocktailFilterRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CocktailFilterRepository {
-    List<CocktailInfo> findSearchFilter(Pageable pageable, List<String> keywordName, Integer alcoholLevel, Integer alcoholLevelRange, List<String> drinkName);
+    List<CocktailInfo> findSearchFilter(Pageable pageable, List<String> keywordName, Integer minAlcoholLevel, Integer maxAlcoholLevel, List<String> drinkName);
 }
