57acfa7b00114de2b6648841141524c55b55f29f
BeobSik <tjswl756@naver.com>
2022-02-11 17:39:49 +0900

    feat: add exposedHeaders

--- src/main/java/com/cocktail_dakk/config/WebConfig.java
+++ src/main/java/com/cocktail_dakk/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
+                .exposedHeaders("Content-Type")
                 .allowedMethods("*");
     }
 }
