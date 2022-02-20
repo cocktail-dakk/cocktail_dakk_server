562831fbd7981487a35e86c4babe833373be4295
BeobSik <tjswl756@naver.com>
2022-02-20 19:18:59 +0900

    style: add annotation at writeTokenResponse

--- src/main/java/com/cocktail_dakk/config/auth/jwt/OAuth2SuccessHandler.java
+++ src/main/java/com/cocktail_dakk/config/auth/jwt/OAuth2SuccessHandler.java
@@ -35,7 +35,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     }
 
     private void writeTokenResponse(HttpServletResponse response, Token token) throws IOException{
-        response.setContentType("text/html;charset=UTF-8");
+//        response.setContentType("text/html;charset=UTF-8");
 
         response.addHeader("Auth", token.getToken());
         response.addHeader("Refresh", token.getRefreshToken());
